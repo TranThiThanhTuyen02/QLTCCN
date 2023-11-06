1d26cea6c9b784f37e0bb85e1ab0a85e41c22cd4
TuyenNeee <112460147+TranThiThanhTuyen02@users.noreply.github.com>
2023-11-06 12:23:11 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,57 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light ct-header">
+        <a class="navbar-brand" href="#">
+            <img class="logo" width="150px" height="80px" src="./assets/img/logo.png" alt="logo">
+        </a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Trang Chủ</button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Tài Khoản</button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">
+                            Quản Lý Thu Chi
+                        </button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Báo Cáo</button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Cảnh Báo Chi Tiêu</button>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="btn btn-outline-secondary ct-font">Chức Năng Khác </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Lập Kế Hoạch Dự Chi</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <!-- <div class="dropdown-divider"></div> -->
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="user">
+            <div class="ct-name">
+                <p>Nguyen Van A</p>
+            </div>
+            <div class="avt">
+                <img src="./assets/img/noimg.jpg" alt="logo">
+            </div>
+        </div>
+
+    </nav>
+</header>
